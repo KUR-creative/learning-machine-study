d13a24478ebc229333269e9128cb6f97bab56cad
kur <rhdnfka94@gmail.com>
2018-01-09 06:23:00 -0800

    complete computing cost!

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -12,11 +12,12 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
-
-
-
+%size(theta)
+%size(X)
+%size(y)
+%size(X*theta)
+%size(X*theta - y)
+J = sum((X*theta - y) .^ 2) / (2 * m)
 % =========================================================================
 
 end
