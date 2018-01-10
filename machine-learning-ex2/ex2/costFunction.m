645a5796848440102285db67aeff85c2dec58fed
kur <rhdnfka94@gmail.com>
2018-01-10 10:17:38 -0800

    add gradient of cost function

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -27,6 +27,7 @@ size(grad);
 
 J = (1/m) * sum(-y .* log(sigmoid(X*theta)) 
                 -(1 .- y) .* log(1 .- sigmoid(X*theta)))
+grad = (1/m) * X' * (sigmoid(X*theta) - y)
 
 % =============================================================
 
