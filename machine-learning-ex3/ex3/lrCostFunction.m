152403df0946e38bb5080a182631cb354df080b9
kur <rhdnfka94@gmail.com>
2018-01-12 15:43:50 +0900

    add cost function and gradient

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,14 +36,12 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+J = (1/m) * sum(-y .* log(sigmoid(X*theta))
+                -(1 .- y) .* log(1 .- sigmoid(X*theta)));
+J = J + (lambda/(2*m)) * sum(theta(2:end).^2);
 
-
-
-
-
-
-
-
+regularizing_factor = [0; (lambda/m)*theta(2:end)];
+grad = (1/m) * X' * (sigmoid(X*theta) - y) + regularizing_factor;
 
 % =============================================================
 
